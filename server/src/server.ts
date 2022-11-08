7d8fdda0dc982a684bc3a74987182c437ff41c26
Emanuel <emanuelzsmj@gmail.com>
2022-11-08 15:53:29 -0300

    NLW Copa do Mundo - Dia 04

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,13 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
-import { z } from 'zod'
-import ShortUniqueId from 'short-unique-id'
+import { poolRoutes } from './routes/pools'
+import { authRoutes } from './routes/auth'
+import { gameRoutes } from './routes/game'
+import { guessRoutes } from './routes/guess'
+import { userRoutes } from './routes/users'
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+
 
 async function bootstrap(){
   const fastify = Fastify({
@@ -18,46 +19,18 @@ async function bootstrap(){
     origin : true,
   })
   
-  fastify.get('/pools/count' , async () => {
-    const count = await prisma.pool.count()
-    
-    return { count}
+  await fastify.register(jwt , {
+    secret : 'nlwcopa',
   })
 
-  fastify.get('/users/count' , async () => {
-    const count = await prisma.user.count()
-    
-    return { count}
-  })
 
-  fastify.get('/guesses/count' , async () => {
-    const count = await prisma.guess.count()
-    
-    return { count}
-  })
+  await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(userRoutes)
 
-  fastify.post('/pools' , async (request , response) => {
-    const createPoolBody = z.object({
-      title: z.string(),
-
-    })
-    
-    const { title } = createPoolBody.parse(request.body)
-
-    const generate = new ShortUniqueId({length: 6})
-    const code = String(generate()).toUpperCase()
-
-    await prisma.pool.create({
-      data: {
-        title,
-        code: String(generate()).toUpperCase()
-      }
-    })
-    
-    return response.status(201).send({code})
-  })
-
-  await fastify.listen({port : 3333  })
+  await fastify.listen({port : 3333, host:'0.0.0.0'  })
 }
 
 
